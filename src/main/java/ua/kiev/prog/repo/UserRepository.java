38559ad0406586a1714dfda27ecf36310fdcb19a
Michael Gorduz <pipsikclub@gmail.com>
2023-09-05 22:36:35 -0600

    initial commit

---
+++ src/main/java/ua/kiev/prog/repo/UserRepository.java
@@ -0,0 +1,22 @@
+package ua.kiev.prog.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import ua.kiev.prog.model.User;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u WHERE u.notified = false " +
+            "AND u.phone IS NOT NULL AND u.email IS NOT NULL")
+    List<User> findNewUsers();
+
+    User findByChatId(long id);
+
+    @Modifying
+    @Query("DELETE FROM User u WHERE u.email = :email")
+    void deleteByEmail(@Param("email") String email);
+}
